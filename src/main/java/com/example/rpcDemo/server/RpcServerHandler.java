54d3bf14eb1fa86fa96bf2b5ebf5fe808d2405c9
colania <465533104@qq.com>
2022-05-15 19:14:38 +0800

    update

--- src/main/java/com/example/rpcDemo/server/RpcServerHandler.java
+++ src/main/java/com/example/rpcDemo/server/RpcServerHandler.java
@@ -18,7 +18,7 @@ public class RpcServerHandler extends ChannelInboundHandlerAdapter {
         //反馈
         Response request = new Response();
         request.setRequestId(msg.getRequestId());
-        request.setParam(msg.getResult() + " 请求成功，反馈结果请接受处理｛公众号：bugstack虫洞栈 博客栈：https://bugstack.cn｝。");
+        request.setParam(msg.getResult() + " 请求成功。");
         ctx.writeAndFlush(request);
         //释放
         ReferenceCountUtil.release(msg);
